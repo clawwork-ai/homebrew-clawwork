b4c1e578fdcf73fbc274695f6407337c02af34b4
samzong <samzong.lu@gmail.com>
2026-03-15 13:31:16 +0800

    chore: update clawwork cask for v0.0.4

--- Casks/clawwork.rb
+++ Casks/clawwork.rb
@@ -1,8 +1,8 @@
 cask "clawwork" do
-  version "0.0.3"
-  sha256 "029fabdf3a5a8c550cb68818af4b18ef9903159c4e32dbfb922827ece55b7970"
+  version "0.0.4"
+  sha256 "1d1c2a60a5ea5c49d12f26c25743e36062cc02ba880bd2047273bf15e7ca8590"
 
-  url "https://github.com/clawwork-ai/clawwork/releases/download/v0.0.3/ClawWork-0.0.3-mac-universal.dmg"
+  url "https://github.com/clawwork-ai/clawwork/releases/download/v0.0.4/ClawWork-0.0.4-mac-universal.dmg"
   name "ClawWork"
   desc "Desktop client for OpenClaw"
   homepage "https://github.com/clawwork-ai/clawwork"
